7ee609cda5268b1aa56f959d78aacb70bba85e9b
NguyenHuyCao <caohk001asd@gmail.com>
2024-10-21 23:13:30 +0700

    Config Session to Server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function HomePage() {
-  // const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
-  //   method: "POST",
-  //   headers: { "Content-Type": "application/json" },
-  //   body: JSON.stringify({
-  //     category: "CHILL",
-  //     limit: 10,
-  //   }),
-  // });
-
-  // console.log("check", res.json());
+  const session = await getServerSession(authOptions);
 
   const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
